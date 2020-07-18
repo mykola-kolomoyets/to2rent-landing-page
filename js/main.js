b64723d371d4d153c0156369c4a4a8a20c71fb8a
Kolomoyets473 <kolomoyets473@gmail.com>
2020-07-18 14:55:38 +0300

    v0.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+	$(".slider").slick({
+		nextArrow: '<button class="slick-arrow slick-next"><img src="./images/next-arrow.svg" alt="next"></button>',
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="./images/prev-arrow.svg" alt="previous"></button>',
+		autoplay: true,
+	});
 });
+
+
